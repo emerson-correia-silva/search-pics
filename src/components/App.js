4dfc31d5b6b51532d848626c3410fc54dc8a6317
emerson-correia-silva <emerson.correia.silva@gmail.com>
2020-05-02 15:36:35 -0300

    App search-pic 03

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    state = { images: [ ] };
+
+
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 
+                    'Client-ID x3w-gU6bhmCrekTosZruJ0cZxFEUiMunBOM4tihSYcM' 
+            }
+        });
+
+        console.log(this);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.lenght} images
             </div>
         );
     }
