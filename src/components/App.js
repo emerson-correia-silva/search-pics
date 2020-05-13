cd642588f75520e3b590ff600adf2363e8cc36d9
emerson-correia-silva <emerson.correia.silva@gmail.com>
2020-05-12 22:06:26 -0300

    commit v02

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import unsplash from '../api/unsplash.js';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
 
 class App extends React.Component {
     state = { images: [] };
@@ -10,14 +12,14 @@ class App extends React.Component {
             params: { query: term }
         });
 
-        this.setState({ images: response.data.results }); 
+        this.setState({ images: response.data.results });
     };
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit} guesswhtIam="im am teh ecs"/>
-                Found: {this.state.images.length} images
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         );
     }
